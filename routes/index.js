12287a58239f4aa06e5efeddfe5f319a5f5056ee
praveendev0327 <praveensubramaniyandev@gmail.com>
2024-07-12 22:25:41 +0530

    update route createproduct

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getAllUsers, createUser, getUserById, getSerpApi } from "../handlers/index.js";
+import { getAllUsers, createUser, getUserById, getSerpApi, createProduct } from "../handlers/index.js";
 
 const appRouter = Router();
 
 appRouter.get("/api/users", getAllUsers)
 appRouter.get("/api/users/serpapi/:id", getSerpApi)
 appRouter.post("/api/users/create", createUser)
+appRouter.post("/api/users/createProduct", createProduct)
 appRouter.get("/api/users/:id", getUserById)
 
 export default appRouter;
