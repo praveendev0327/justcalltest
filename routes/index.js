5bdab464d6d1e97e096a80aafe6dcb1d7be34b83
praveendev0327 <praveensubramaniyandev@gmail.com>
2024-07-15 12:32:39 +0530

    Update index.js login

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllUsers, createUser, getUserById, getSerpApi, createProduct, createItemPurchase } from "../handlers/index.js";
+import { getAllUsers, createUser, getUserById, getSerpApi, createProduct, createItemPurchase, loginUser } from "../handlers/index.js";
 
 const appRouter = Router();
 
 appRouter.get("/api/users", getAllUsers)
 appRouter.get("/api/users/serpapi/:id", getSerpApi)
+appRouter.post("/api/users/login", loginUser)
 appRouter.post("/api/users/create", createUser)
 appRouter.post("/api/users/createProduct", createProduct)
 appRouter.post("/api/users/createItemPurchase", createItemPurchase)
