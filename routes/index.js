3980d5674dee7eddd5a5bc164ae26e5cf09f812e
praveendev0327 <praveensubramaniyandev@gmail.com>
2024-07-15 23:27:06 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,30 @@
 import { Router } from "express";
-import { getAllUsers, createUser, getUserById, getSerpApi, createProduct, createItemPurchase, loginUser } from "../handlers/index.js";
+import { getAllUsers, createUser, getUserById, getSerpApi, createProduct, createItemPurchase, loginUser, getSubGroupList, getAllSubGroupList, updateProduct } from "../handlers/index.js";
+
+import multer from "multer";
+import path from "path";
 
 const appRouter = Router();
 
+const storage = multer.diskStorage({
+    destination : "./upload/images",
+    filename : (req, file, cb) =>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({
+    storage : storage
+})
+
+
 appRouter.get("/api/users", getAllUsers)
+appRouter.get("/api/users/allCategoryList/:SubGroupName", getAllSubGroupList)
+appRouter.get("/api/users/subGroupList", getSubGroupList)
 appRouter.get("/api/users/serpapi/:id", getSerpApi)
 appRouter.post("/api/users/login", loginUser)
 appRouter.post("/api/users/create", createUser)
+appRouter.put("/api/users/updateProduct",upload.single("image"), updateProduct)
 appRouter.post("/api/users/createProduct", createProduct)
 appRouter.post("/api/users/createItemPurchase", createItemPurchase)
 appRouter.get("/api/users/:id", getUserById)
