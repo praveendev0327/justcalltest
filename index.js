332cd44588e19b13c1a1a682e533f0dbe39d0183
praveendev0327 <praveensubramaniyandev@gmail.com>
2024-07-15 23:27:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import appRouter from "./routes/index.js";
 import { connectToDatabase } from "./db/index.js"
+import multer from "multer";
 import cors from "cors"
+
+const upload = multer({
+    dest: './upload/images'
+})
 
 const app = express();
 
@@ -11,6 +16,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/",appRouter)
+
+app.post("/upload",upload.single('profile'),(req,res) => {
+      console.log(req.file);
+})
+
+app.use('/productimages', express.static('./upload/images'))
 
 const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0'; 
