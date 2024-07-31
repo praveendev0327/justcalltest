c4f4dac70ef9c2b2c7d8383146307fd8f956e225
praveendev0327 <praveensubramaniyandev@gmail.com>
2024-08-01 00:01:15 +0530

    Update index.js

--- handlers/index.js
+++ handlers/index.js
@@ -1,4 +1,4 @@
-import { findAll, findUserById, addUser, addProduct, addItemPurchase, loginUserQuery, subGroupList, getAllSubGroupListQuery, updateProductQuery, updateProductImage, addOfferProduct, getAllOffersQuery, updateOffersProduct, getOffersByLimitQuery, createProfileMVQuery, getProfileByIdQuery, getProfileByWorkQuery, addBanners, getProfileByEmailQuery, addJobPostQuery, addCvQuery, getJobPostByEmailQuery,  deleteOfferByIdQuery, getCvByEmailQuery, addMemberQuery, loginMemberQuery, getAllJobPostQuery, addDeliveryListQuery, getAllDeliveryListQuery } from "../db/queries.js";
+import { findAll, findUserById, addUser, addProduct, addItemPurchase, loginUserQuery, subGroupList, getAllSubGroupListQuery, updateProductQuery, updateProductImage, addOfferProduct, getAllOffersQuery, updateOffersProduct, getOffersByLimitQuery, createProfileMVQuery, getProfileByIdQuery, getProfileByWorkQuery, addBanners, getProfileByEmailQuery, addJobPostQuery, addCvQuery, getJobPostByEmailQuery,  deleteOfferByIdQuery, getCvByEmailQuery, addMemberQuery, loginMemberQuery, getAllJobPostQuery, addDeliveryListQuery, getAllDeliveryListQuery, updateDeliveryStatusQuery } from "../db/queries.js";
 import bodyParser from "body-parser";
 import https from "https";
 import axios from "axios";
@@ -240,6 +240,21 @@ export const updateImage = async (req, res) => {
   } catch (error) {
     console.log("Error in updateImage");
     return res.status(500).json({ message: "Error in updateImage" });
+  }
+};
+
+export const updateDeliveryStatus = async (req, res) => {
+  const {status, serialno, id  } = req.body;
+  // const img = req.file.filename;
+  // console.log("barcode",img);
+  try {
+    const data = await updateDeliveryStatusQuery(status, serialno, id );
+    // console.log(data);
+    return res.status(200).json({ data });
+    
+  } catch (error) {
+    console.log("Error in updateDeliveryStatus");
+    return res.status(500).json({ message: "Error in updateDeliveryStatus" });
   }
 };
 
