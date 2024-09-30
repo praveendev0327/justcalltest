b5cc714991bc7ca52208c97247c906df0f2e6d11
praveendev0327 <praveensubramaniyandev@gmail.com>
2024-09-30 16:16:43 +0530

    Update index.js getAllSectionProducts

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUsers, createUser, getUserById, getSerpApi, createProduct, createItemPurchase, loginUser, getSubGroupList, getAllSubGroupList, updateProduct, updateImage, addOffers, getAllOffers, updateOffers, getOffersByLimit, createProfileMV, getProfileById, getProfileByWork, createBannersMV, getProfileByEmail, createJobPostMV, createCvMV, getJobPostByEmail,  deleteOfferById, getCvByEmail, createMemberMV, loginMember, getAllJobPostMV, createDeliveryList, getAllDeliverylist, updateDeliveryStatus, updateDeliveryAppToken, getDeliveryAppToken, registerCustomer, loginCustomer, updateAccountStatus } from "../handlers/index.js";
+import { getAllUsers, createUser, getUserById, getSerpApi, createProduct, createItemPurchase, loginUser, getSubGroupList, getAllSubGroupList, updateProduct, updateImage, addOffers, getAllOffers, updateOffers, getOffersByLimit, createProfileMV, getProfileById, getProfileByWork, createBannersMV, getProfileByEmail, createJobPostMV, createCvMV, getJobPostByEmail,  deleteOfferById, getCvByEmail, createMemberMV, loginMember, getAllJobPostMV, createDeliveryList, getAllDeliverylist, updateDeliveryStatus, updateDeliveryAppToken, getDeliveryAppToken, registerCustomer, loginCustomer, updateAccountStatus, getAllSections, addNewSections, addSectionProducts, updateSections, updateSectionProducts, getAllSectionProducts } from "../handlers/index.js";
 
 import multer from "multer";
 import path from "path";
@@ -20,6 +20,8 @@ const upload = multer({
 
 appRouter.get("/api/users", getAllUsers)
 appRouter.get("/api/offers", getAllOffers)
+appRouter.get("/api/sections", getAllSections)
+appRouter.get("/api/sectionproducts", getAllSectionProducts)
 appRouter.get("/api/getDeliveryAppToken", getDeliveryAppToken)
 appRouter.get("/api/deliverylist", getAllDeliverylist)
 appRouter.get("/api/users/allCategoryList/:SubGroupName", getAllSubGroupList)
@@ -44,12 +46,16 @@ appRouter.post("/api/users/createCvMV", createCvMV)
 appRouter.post("/api/users/createProfileMV", createProfileMV)
 appRouter.post("/api/users/createMemberMV", createMemberMV)
 appRouter.post("/api/users/offers", addOffers)
+appRouter.post("/api/users/addnewsections", addNewSections)
+appRouter.post("/api/users/addsectionproducts", addSectionProducts)
 appRouter.put("/api/users/updateProduct",upload.single("image"), updateProduct)
 appRouter.put("/api/users/updateProductImage", updateImage)
 appRouter.put("/api/users/updateDeliveryStatus", updateDeliveryStatus)
 appRouter.put("/api/users/updateAccountStatus", updateAccountStatus)
 appRouter.put("/api/users/updateDeliveryAppToken", updateDeliveryAppToken)
 appRouter.put("/api/users/updateOffersProduct", updateOffers)
+appRouter.put("/api/users/updateSections", updateSections)
+appRouter.put("/api/users/updateSectionProducts", updateSectionProducts)
 appRouter.post("/api/users/createProduct", createProduct)
 appRouter.post("/api/users/createItemPurchase", createItemPurchase)
 appRouter.get("/api/users/:id", getUserById)
